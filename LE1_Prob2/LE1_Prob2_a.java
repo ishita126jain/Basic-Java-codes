a44c7f3017dd729ba8705bd9f44bab2a9502b49c
ishita <ishita126jain@gmail.com>
2022-05-31 00:28:27 +0530

    modification

--- LE1_Prob2/LE1_Prob2_a.java
+++ LE1_Prob2/LE1_Prob2_a.java
@@ -16,18 +16,3 @@ for(int i=0;i<4;i++)
 
 
 
-public class Main
-{
-public static void main(String[] args) {
-for(int i=4;i>0;i--)
-    {
-        for(int k=0;k<4-i;k++)
-            System.out.print(" ");
-        for(int j=4;j>4-i;j--)
-        {
-            System.out.print("* ");
-        }
-        System.out.print("\n");
-    }
-}
-}
